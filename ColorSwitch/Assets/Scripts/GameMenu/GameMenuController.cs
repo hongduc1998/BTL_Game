69b162ab0d4c2aaad48a96fd3e8daea1ec2c719f
hongduc1998 <hong.duc.1406@gmail.com>
2019-11-16 23:28:23 +0700

    Add GameMenu Scence

---
+++ ColorSwitch/Assets/Scripts/GameMenu/GameMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuController : MonoBehaviour
+{
+
+    public static GameMenuController instance;
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ColorSwitch/Assets/Scripts/GameMenu/MenuScenceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScenceController : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip pressButton;
+    public void PlayButton()
+    {
+        audioSource.PlayOneShot(pressButton);
+        SceneManager.LoadScene("GamePlay");
+    }
+}
