2d2d69e28ff54341f7f112ba34562a259e9846dd
hongduc1998 <hong.duc.1406@gmail.com>
2019-11-16 23:49:54 +0700

    add something to GamePlay

---
+++ ColorSwitch/Assets/Scripts/GamePlay/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float rotateSpeed;
+    private Rigidbody2D playerRb;
+    // Use this for initialization
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerRb.velocity = Vector2.up * jumpForce;
+        }
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+}
